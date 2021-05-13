b33e404fad4ae197d649df319a91ba9b2a90bd4b
quangduy1810 <quangduy18102000@gmail.com>
2021-05-14 05:52:38 +0700

    update connection again

--- Greensys.py
+++ Greensys.py
@@ -1,5 +1,25 @@
-from flask import Flask, render_template
-
-app = Flask(__name__, template_folder= "static")
-
+from flask import Flask, render_template, request
+from flask_mysqldb import MySQL
+import views
 import os
+import cx_Oracle
+cx_Oracle.init_oracle_client(lib_dir=r"D:\coder\oracle\instantclient_19_11")
+def querydb():
+    con = cx_Oracle.connect('username/password@x.x.x.x:1521/Servicename')    
+    pn = request.form['phonenumber']
+    cur = con.cursor()
+    sql = """select to_char(DTCREAT,'yyyy-mm-dd hh24:mi:ss') ..... and numero = :numero"""
+    cur.execute(sql, numero = pn)
+    title = [i[0] for i in cur.description]
+    print(title)
+    results = []
+    for result in cur.fetchall():
+         results.append(result)
+    return render_template('query.html', results= results)
+app = Flask(__name__, template_folder= "static")
+app.config['MYSQL_HOST'] = 'sql6.freemysqlhosting.net'
+app.config['MYSQL_USER'] = 'sql6412254'
+app.config['MYSQL_PASSWORD'] = 'qM4MyknmEg'
+app.config['MYSQL_DB'] = 'sql6412254'
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+mysql=MySQL(app)
--- views.py
+++ views.py
@@ -1,20 +1,57 @@
 from datetime import datetime
 from flask import render_template, request, jsonify
+from flask_mysqldb import MySQL
 from Greensys import app
 import os
 #import Process
 import webbrowser
 import json
 import glob
+mysql=MySQL(app)
+userData = {'id':None,'name':None}
 @app.route('/')
 @app.route('/homepage')
 def homepage():
     return render_template('homepage.html')
 @app.route('/login')
 def login():
+    error = None
+    if request.method == 'POST':
+        username = request.form['username']
+        cur = mysql.connection.cursor()
+        cur.execute("SELECT * FROM account WHERE username = '"+ username + "'")
+        acc = cur.fetchone()
+        if acc is None or acc['password'] != request.form['password']:
+            error = 'Username hoặc mật khẩu không đúng'
+        else:
+            userData['id'] = acc['id']
+            userData['name'] = acc['name']
+
+            cur.execute("SELECT * FROM farm WHERE owner_id = "+ str(userData['id']))
+            acc = cur.fetchone()
+            if acc is not None:
+                userData['id'] = acc['id']
+                acc = None
     return render_template('logIn.html')
-@app.route('/signup')
+@app.route('/signup',methods=['GET','POST'])
 def signup():
+    error = None
+    if request.method == 'POST':
+        username = request.form['username']
+        cur = mysql.connection.cursor()
+        cur.execute("SELECT * FROM account WHERE username = '"+ username +"'")
+        acc = cur.fetchone()
+        if acc is not None:
+            error = 'Tài khoản đã tồn tại'
+        else:
+            name = request.form['name']
+            password = request.form['password']
+            email = request.form['email']
+            phonenumber = request.form['phonenumber']
+            address= request.form['address']
+            cur.execute("INSERT INTO account (id, name, username, password, email) VALUES ('AUTO_INCREMENT PRIMARY KEY','"+name+"','"+username+"','"+password+"','"+email+"')")
+            mysql.connection.commit()
+            return redirect(url_for('account'))
     return render_template('signUp.html')
 @app.route('/plantdata.html')
 def plantdata():
