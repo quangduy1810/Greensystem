d161b963765d0cfdd99a70a033227f116676d8fe
xXHellgate25Xx <55650626+xXHellgate25Xx@users.noreply.github.com>
2021-05-20 00:33:06 +0700

    Add_data.sql and update envicondi.html

---
+++ Add_data.sql
@@ -0,0 +1,7 @@
+INSERT IGNORE INTO device_acted_in_land(DeviceId, LandId, RealTime, measurementUnit, measurementValue) VALUES
+	(1, 1, '2021-05-19 12:50:39', 'Unknown',1),
+	(2, 1, '2021-05-19 13:50:39', 'Celcius', 30),
+	(3, 1, '2021-05-19 14:50:39', 'Unknown', 1),
+	(4, 1, '2021-05-19 15:50:39', 'Percent', 50),
+	(4, 2, '2021-05-19 16:50:39', 'Percent', 60 )
+;
