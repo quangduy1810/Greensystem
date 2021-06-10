39927b243ee40f4066943d50e34d5e74fbbca84d
xXHellgate25Xx <55650626+xXHellgate25Xx@users.noreply.github.com>
2021-06-11 02:19:42 +0700

    Finished front-end for charts
    - Need to finish data processing.

--- static/envicondi.js
+++ static/envicondi.js
@@ -1,3 +1,211 @@
+//Draw Chart.js
+function drawTemperatureChart(labels, data, isAjax) {
+    var temperatureCanvas = document.getElementById("TemperatureChart").getContext("2d");
+    globalThis.temperature_chart;
+    if (typeof temperature_chart == "undefined") {
+        temperature_chart = new Chart(temperatureCanvas, {
+        animationEnabled: true,
+        zoomEnabled: true,
+        title: {
+        text: "Temperature Chart"
+        },
+        axisX: {
+        title: "Date time"
+        },
+        axisY: {
+        title: "Temperature"
+        },
+        data: {
+            labels: labels,
+            datasets: [
+                {
+                label: "Temperature",
+                data: data,
+                fill: false,
+                borderColor: "rgb(255,0,0)",
+                lineTension: 0.1,
+                type: "line"
+                }
+            ]
+            },
+            options: {
+            responsive: true
+            }
+        });
+    }
+    else {
+        temperature_chart.destroy();
+        temperature_chart = new Chart(temperatureCanvas, {
+            animationEnabled: true,
+            zoomEnabled: true,
+            title: {
+            text: "Temperature Chart"
+            },
+            axisX: {
+            title: "Date time"
+            },
+            axisY: {
+            title: "Temperature"
+            },
+            data: {
+                labels: labels,
+                datasets: [
+                    {
+                    label: "Temperature",
+                    data: data,
+                    fill: false,
+                    borderColor: "rgb(255,0,0)",
+                    lineTension: 0.1,
+                    type: "line"
+                    }
+                ]
+                },
+                options: {
+                responsive: true
+                }
+            });
+    }
+  }
+
+  function drawHumidityChart(labels, data) {
+    var humidityCanvas = document.getElementById("HumidityChart").getContext("2d");
+    globalThis.humidity_chart;
+    if ((typeof humidity_chart) == 'undefined') {
+        humidity_chart = new Chart(humidityCanvas, {
+        animationEnabled: true,
+        zoomEnabled: true,
+        title: {
+        text: "Humidity Chart"
+        },
+        axisX: {
+        title: "Date time"
+        },
+        axisY: {
+        title: "Humidity"
+        },
+        data: {
+            labels: labels,
+            datasets: [
+                {
+                label: "Humidity",
+                data: data,
+                fill: false,
+                borderColor: "rgb(0,0,255)",
+                lineTension: 0.1,
+                type: "line"
+                }
+            ]
+            },
+            options: {
+            responsive: true
+            }
+        });
+    }
+    else {
+        humidity_chart.destroy();
+        humidity_chart = new Chart(humidityCanvas, {
+            animationEnabled: true,
+            zoomEnabled: true,
+            title: {
+            text: "Humidity Chart"
+            },
+            axisX: {
+            title: "Date time"
+            },
+            axisY: {
+            title: "Humidity"
+            },
+            data: {
+                labels: labels,
+                datasets: [
+                    {
+                    label: "Humidity",
+                    data: data,
+                    fill: false,
+                    borderColor: "rgb(0,0,255)",
+                    lineTension: 0.1,
+                    type: "line"
+                    }
+                ]
+                },
+                options: {
+                responsive: true
+                }
+            });
+    }
+  }
+
+  function drawBrightnessChart(labels,data) {
+    var brightnessCanvas = document.getElementById("BrightnessChart").getContext("2d");
+    globalThis.brightness_chart;
+    if ((typeof brightness_chart) == 'undefined') {
+        brightness_chart = new Chart(brightnessCanvas, {
+        animationEnabled: true,
+        zoomEnabled: true,
+        title: {
+        text: "Brightness Chart"
+        },
+        axisX: {
+        title: "Date time"
+        },
+        axisY: {
+        title: "Brightness"
+        },
+        data: {
+            labels: labels,
+            datasets: [
+                {
+                label: "Brightness",
+                data: data,
+                fill: false,
+                borderColor: "rgb(210,180,140)",
+                lineTension: 0.1,
+                type: "line"
+                }
+            ]
+            },
+            options: {
+            responsive: true
+            }
+        });
+    }
+    else {
+        brightness_chart.destroy();
+        brightness_chart = new Chart(brightnessCanvas, {
+            animationEnabled: true,
+            zoomEnabled: true,
+            title: {
+            text: "Brightness Chart"
+            },
+            axisX: {
+            title: "Date time"
+            },
+            axisY: {
+            title: "Brightness"
+            },
+            data: {
+                labels: labels,
+                datasets: [
+                    {
+                    label: "Brightness",
+                    data: data,
+                    fill: false,
+                    borderColor: "rgb(210,180,140)",
+                    lineTension: 0.1,
+                    type: "line"
+                    }
+                ]
+                },
+                options: {
+                responsive: true
+                }
+            });
+    }
+  }
+  
+
+//AJAX
+
 $(document).ready(function() {
     $('#form_temperature').on('submit', function(event) {
       event.preventDefault();
@@ -12,14 +220,14 @@ $(document).ready(function() {
         url : '/envicondi2'
       })
       .done(function(data) {
-        alert("Form_temperature");
-        drawTemperatureChart(data.temp_label,data.temp_data);
+        drawTemperatureChart(data.temp_label,data.temp_data,true);
       });
 
     });
 
   $('#form_humidity').on('submit', function(event) {
-      $.ajax({
+    event.preventDefault();
+    $.ajax({
         data : {
           start_humid_date : $('#datetimepicker3').val(),
           end_humid_date : $('#datetimepicker4').val(),
@@ -33,11 +241,11 @@ $(document).ready(function() {
         drawHumidityChart(data.humid_label,data.humid_data);
       });
 
-      event.preventDefault();
     });
 
   $('#form_brightness').on('submit', function(event) {
-      $.ajax({
+    event.preventDefault(); 
+    $.ajax({
         data : {
           start_bright_date : $('#datetimepicker5').val(),
           end_bright_date : $('#datetimepicker6').val(),
@@ -52,6 +260,5 @@ $(document).ready(function() {
         drawBrightnessChart(data.brightness_label,data.brightness_data);
       });
 
-      event.preventDefault();
     });
 });
