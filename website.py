78282becf6881b1d84663d0a2be6517738e60387
xXHellgate25Xx <55650626+xXHellgate25Xx@users.noreply.github.com>
2021-06-14 21:51:45 +0700

    Add envicondi

--- website.py
+++ website.py
@@ -75,16 +75,326 @@ def plantdata():
     session['Plantdata']=data
     print(device)
     return render_template('plantdata.html',data=data,data2=device)
-@app.route('/envicondi')
+
+
+@app.route('/api', methods=['GET','POST'])
+def notify_api(): 
+    
+    res =   "The temperature is : " + notification["temperature"] + "\n" + \
+            "The humidity is : " + notification["humidity"]  + "\n" + \
+            "The brightness is :  " +  notification["brightness"] + "\n" + \
+            str(notification["alert"])
+
+    notification["date"] = datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
+
+    if request.method == 'POST':
+        req = request.json
+
+        notification["temperature"] = req["temperature"]
+        notification["humidity"] = req['humidity']
+        notification["brightness"] = req['brightness']
+        notification["alert"] = req['alert']
+
+        return jsonify(res)
+    else :
+
+        return jsonify(notification)
+
+
+@app.route('/envicondi', methods=['GET'])
 def envicondi():
-    data= {
-        "temperature" : getTemperature().value,
-        "humidity": getHumidity().value,
-        "brightness": getBrightness().value
-    }
-    cur.execute("SELECT * FROM LAND WHERE UserID = '"+ str(session['UserData'][0]) + "'")
-    data2=cur.fetchall()
-    return render_template('envicondi.html',data=data,data2=data2)
+    if request.method == 'GET':
+        cur.execute("SELECT environment_log.id,LandName,measureValue,measureType,CurrentTime,UserID FROM environment_log,land WHERE USERID = '"+ str(session['UserData'][0]) +"' ")
+        data=cur.fetchall()
+        #data = [(),(),(),...]
+        land = data[1][1]
+        # print("Land:" + str(land))
+        temperature_label = []
+        temperature_data = []
+        humidity_label = []
+        humidity_data = []
+        brightness_label = []
+        brightness_data = []
+        current_time = []
+        for value in data:
+            #print(value)
+            if str(value[3]) == 'Temperature':
+                temperature_data.append(value[2])
+                temperature_label.append(str(value[4]))
+            if str(value[3]) == 'Humidity':
+                humidity_data.append(value[2])
+                humidity_label.append(str(value[4]))
+            if str(value[3]) == 'Brightness':
+                brightness_data.append(value[2])
+                brightness_label.append(str(value[4]))
+            if str(value[4]) not in current_time:
+                current_time.append(str(value[4]))
+        
+        # print("Temperature length: "+ str(len(temperature_data)))
+        # print("Humidity length: "+ str(len(humidity_data)))
+        # print("Brightness length: "+ str(len(brightness_data)))
+        # print("Current_time length: "+ str(len(current_time)))
+
+        # print("Temperature: "+ str(temperature_data))
+        # print("Humidity: "+ str(humidity_data))
+        # print("Brightness:"+ str(brightness_data))
+        # print("CurrentTime"+ str(current_time))
+
+        return render_template('envicondi.html',land=land, temperature_label=temperature_label , temperature_data=temperature_data, humidity_label=humidity_label , humidity_data=humidity_data, brightness_label=brightness_label , brightness_data=brightness_data, current_time=current_time)
+
+def average_data(label,data,option):
+    process_label = []
+    process_data = []
+    start = label[0]
+    end = label[-1]
+    index = 0
+    if option == 'seconds':
+        process_label = label
+        process_data = data
+    if option == 'minutes':
+        while start <= end:
+            
+            next_minute = start
+            if start.minute < 59:
+                next_minute = start.replace(minute= start.minute + 1)
+            elif start.hour < 23:
+                next_minute = start.replace(hour= start.hour + 1, minute= 0)
+            elif start.day < 31 and start.month in (1,3,5,7,8,10,12):
+                next_minute = start.replace(day= start.day + 1, hour=0)
+            elif start.day < 30 and start.month in (2,4,6,9,11):
+                next_minute = start.replace(day= start.day +1, hour=0)
+            elif start.month < 12:
+                next_minute = start.replace(month= start.month + 1, day=1)
+            else:
+                next_minute = start.replace(year= start.year + 1, month=1)
+            
+            #print(next_minute)
+
+            temp_data = []
+            for i in range(0,len(label)):
+                if label[i] >= start and label[i] <= next_minute:
+                    temp_data.append(data[i])
+            if temp_data != []:
+                process_label.append(start)
+                process_data.append(sum(temp_data) / len(temp_data))
+            start = next_minute
+            index += 1
+    if option == 'hours':
+        while start <= end:
+            
+            next_hour = start
+            if start.hour < 23:
+                next_hour = start.replace(hour= start.hour + 1, minute= 0)
+            elif start.day < 31 and start.month in (1,3,5,7,8,10,12):
+                next_hour = start.replace(day= start.day + 1, hour=0)
+            elif start.day < 30 and start.month in (2,4,6,9,11):
+                next_hour = start.replace(day= start.day +1, hour=0)
+            elif start.month < 12:
+                next_hour = start.replace(month= start.month + 1, day=1)
+            else:
+                next_hour = start.replace(year= start.year + 1, month=1)
+            
+            temp_data = []
+            for i in range(index,len(label)):
+                if label[i] >= start and label[i] <= end:
+                    temp_data.append(data[i])
+            if temp_data != []:
+                process_label.append(start)
+                process_data.append(sum(temp_data) / len(temp_data))
+            start = next_hour
+            index += 1    
+    if option == 'days':
+        while start <= end:
+
+            next_day = start
+            if start.day < 31 and start.month in (1,3,5,7,8,10,12):
+                next_day = start.replace(day= start.day + 1, hour=0)
+            elif start.day < 30 and start.month in (2,4,6,9,11):
+                next_day = start.replace(day= start.day +1, hour=0)
+            elif start.month < 12:
+                next_day = start.replace(month= start.month + 1, day=1)
+            else:
+                next_day = start.replace(year= start.year + 1, month=1)
+            
+            temp_data = []
+            for i in range(index,len(label)):
+                if label[i] >= start and label[i] <= end:
+                    temp_data.append(data[i])
+            if temp_data != []:
+                process_label.append(start)
+                process_data.append(sum(temp_data) / len(temp_data))
+            start = next_day
+            index += 1    
+    if option == 'months':
+        while start <= end:
+            next_month = start
+            if start.month < 12:
+                next_day = start.replace(month= start.month + 1, day=1)
+            else:
+                next_day = start.replace(year= start.year + 1, month=1)
+
+            temp_data = []
+            for i in range(index,len(label)):
+                if label[i] >= start and label[i] <= end:
+                    temp_data.append(data[i])
+            if temp_data != []:
+                process_label.append(start)
+                process_data.append(sum(temp_data) / len(temp_data))
+            start = next_month
+            index += 1   
+    if option == 'years':
+        while start <= end:
+            next_year = start.replace(year= start.year + 1)
+            temp_data = []
+            for i in range(index,len(label)):
+                if label[i] >= start and label[i] <= end:
+                    temp_data.append(data[i])
+            if temp_data != []:
+                process_label.append(start)
+                process_data.append(sum(temp_data) / len(temp_data))
+            start = next_year
+            index += 1   
+    return process_label,process_data
+
+def process_data(label,data, start_time, end_time, option):
+    #label is of datetime
+    new_label = []
+    new_data = []
+
+    process_label = []
+    process_data = []
+
+    process_label,process_data = average_data(label,data,option)
+
+    for i in range(0,len(process_label)):
+        if start_time <= process_label[i] and end_time >= process_label[i]:
+            new_label.append(process_label[i].strftime("%Y-%m-%d %H:%M"))
+            new_data.append(process_data[i])        
+
+    return new_label,new_data
+
+@app.route('/envicondi2', methods=["POST"])
+def envicondi2():
+    #Data to be sent back
+    temp_label = []
+    temp_data = []
+
+    humid_label = []
+    humid_data = []
+
+    brightness_label = []
+    brightness_data = []
+
+    start_temp_date = None
+    end_temp_date = None
+    start_humid_date = None
+    end_humid_date = None
+    start_bright_date = None
+    end_bright_date = None
+
+    #Get data from database
+    cur.execute("SELECT environment_log.id,LandName,measureValue,measureType,CurrentTime,UserID FROM environment_log,land WHERE USERID = '"+ str(session['UserData'][0]) +"' ")
+    data=cur.fetchall()
+    #data = [(),(),(),...]
+    temperature_data = [[],[]]
+    humidity_data = [[],[]]
+    brightness_data = [[],[]]
+    for value in data:
+        if str(value[3]) == 'Temperature':
+            temperature_data[0].append(value[2])
+            temperature_data[1].append(value[4])
+        if str(value[3]) == 'Humidity':
+            humidity_data[0].append(value[2])
+            humidity_data[1].append(value[4])
+        if str(value[3]) == 'Brightness':
+            brightness_data[0].append(value[2])
+            brightness_data[1].append(value[4])
+    #
+    #Testing purpose
+    #print("temperature_data[[],[]] length :" + str(len(temperature_data[0])) + " , " + str(len(temperature_data[1])))
+
+    #Process Temperature submit
+    if (request.form.get('form_type') == 'Temperature' ):
+        #They are all text data
+        if (request.form.get('start_temp_date')):
+            start_temp_date = request.form['start_temp_date']
+            #print(start_temp_date)
+            #print(type(start_temp_date))
+            #Convert to datetime
+            start_temp_date = datetime.strptime(start_temp_date, '%Y-%m-%d %H:%M')
+
+        if (request.form.get('end_temp_date')):
+            end_temp_date = request.form['end_temp_date']
+            #Convert to datetime
+            end_temp_date = datetime.strptime(end_temp_date, '%Y-%m-%d %H:%M')
+        #Get Option data
+        if (request.form.get('option_temp')):
+            option_temp = request.form['option_temp']
+        
+        #Process data
+        if ( (start_temp_date == None) and ( end_temp_date != None) ):
+            temp_label,temp_data = process_data(temperature_data[1],temperature_data[0],temperature_data[1][0],end_temp_date,option_temp)
+        elif ( (start_temp_date != None) and (end_temp_date == None) ):
+            temp_label,temp_data = process_data(temperature_data[1],temperature_data[0],start_temp_date,temperature_data[1][-1],option_temp)
+        elif ( (start_temp_date == None) and (end_temp_date == None) ):
+            temp_label,temp_data = process_data(temperature_data[1],temperature_data[0],temperature_data[1][0],temperature_data[1][-1],option_temp)
+        else:
+            temp_label,temp_data = process_data(temperature_data[1],temperature_data[0],start_temp_date,end_temp_date,option_temp)
+
+    #Process Humidity submit
+    if (request.form.get('form_type') == 'Humidity'):
+        if (request.form['start_humid_date']):
+            start_humid_date = request.form['start_humid_date']
+            #Convert to datetime
+            start_humid_date = datetime.strptime(start_humid_date, '%Y-%m-%d %H:%M')
+        if (request.form['end_humid_date']):
+            end_humid_date = request.form['end_humid_date']
+            #Convert to datetime
+            end_humid_date = datetime.strptime(end_humid_date, '%Y-%m-%d %H:%M')
+        if (request.form['option_humid']):
+            option_humid = request.form['option_humid']
+        
+        #Process data
+        if ( (start_humid_date == None ) and ( end_humid_date != None) ):
+            humid_label,humid_data = process_data(humidity_data[1],humidity_data[0],humidity_data[1][0],end_humid_date,option_humid)
+        elif ( (start_humid_date != None) and (end_humid_date == None) ):
+            humid_label,humid_data = process_data(humidity_data[1],humidity_data[0],start_humid_date,humidity_data[1][-1],option_humid)
+        elif ( (start_humid_date == None) and (end_humid_date == None) ):
+            humid_label,humid_data = process_data(humidity_data[1],humidity_data[0],humidity_data[1][0],humidity_data[1][-1],option_humid)
+        else:
+            humid_label,humid_data = process_data(humidity_data[1],humidity_data[0],start_humid_date,end_humid_date,option_humid)
+
+    #Process Brightness submit
+    if (request.form.get('form_type') == 'Brightness'):
+        if (request.form['start_bright_date']):
+            start_bright_date = request.form['start_bright_date']
+            #Convert to datetime
+            start_bright_date = datetime.strptime(start_bright_date, '%Y-%m-%d %H:%M')
+        if (request.form['end_bright_date']):
+            end_bright_date = request.form['end_bright_date']
+            #Convert to datetime
+            end_bright_date = datetime.strptime(end_bright_date, '%Y-%m-%d %H:%M')    
+        if (request.form['option_bright']):
+            option_bright = request.form['option_bright']
+
+        #Process data
+
+        if ( (start_bright_date == None) and (end_bright_date != None) ):
+            brightness_label,brightness_data = process_data(brightness_data[1],brightness_data[0],brightness_data[1][0],end_bright_date,option_bright)
+        elif ( (start_bright_date != None) and (end_bright_date == None) ):
+            brightness_label,brightness_data = process_data(brightness_data[1],brightness_data[0],start_bright_date,brightness_data[1][-1],option_bright)
+        elif ( (start_bright_date == None) and (end_bright_date == None) ):
+            brightness_label,brightness_data = process_data(brightness_data[1],brightness_data[0],brightness_data[1][0],brightness_data[1][-1],option_bright)
+        else:
+            brightness_label,brightness_data = process_data(brightness_data[1],brightness_data[0],start_bright_date,end_bright_date,option_bright)
+        
+
+
+        #return
+    return jsonify({'temp_label': temp_label, 'temp_data': temp_data, 'humid_label': humid_label, 'humid_data': humid_data, 'brightness_label': brightness_label, 'brightness_data': brightness_data})
+
+
+
 @app.route('/wateringhistory')
 def wateringhistory():
     cur.execute("SELECT * FROM LAND WHERE UserID = '"+ str(session['UserData'][0]) + "'")
